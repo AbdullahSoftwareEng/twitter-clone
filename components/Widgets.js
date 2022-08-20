307d48cbb8fb420fa48b9ab7facc3bce4149565d
Abdullah Hasan <abdullah.software.engineer@gmail.com>
2022-08-20 04:29:52 +0300

    create the news section of widgets component

--- components/Widgets.js
+++ components/Widgets.js
@@ -1,6 +1,9 @@
 import { SearchIcon } from "@heroicons/react/outline"
+import { useState } from "react"
+import News from "./News"
 
-const Widgets = () => {
+const Widgets = ({newsResults}) => {
+    const [articleNum, setArticleNum] = useState(3);
   return (
     <div className="xl:w-[600px] hidden lg:inline ml-8 space-y-5">
         {/* Search Bar */}
@@ -34,6 +37,38 @@ const Widgets = () => {
                                bg-gray-100"/>
             </div>
         </div>
+
+        <div className=" text-gray-700 
+                         space-y-3 
+                         bg-gray-100
+                         rounded-xl
+                         pt-2
+                         w-[90%]
+                         xl:w-[75%]">
+            
+            <h4 
+                className="font-bold 
+                           text-xl
+                           px-4
+                           mb-4">
+                What's happening
+            </h4>
+            {newsResults.slice(0,articleNum).map(
+                article =>(
+                    <News key={article.title} article={article}/>
+                )
+             )}
+            <button 
+                onClick={() => setArticleNum((prev => prev + 3 ))}
+                className="text-blue-300 
+                            pl-4 
+                            pb-3 
+                            hover:text-blue-400">
+                Show more
+            </button> 
+        </div>
+
+       
     </div>
   )
 }
