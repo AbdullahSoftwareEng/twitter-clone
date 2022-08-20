5ea411ee76bf87402a2e143663de93aeeca28f0d
Abdullah Hasan <abdullah.software.engineer@gmail.com>
2022-08-20 03:32:28 +0300

    create the widgets component and the search bar

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Sidebar from '../components/Sidebar'
 import Feed from '../components/Feed'
+import Widgets from '../components/Widgets'
 
 export default function Home() {
   return (
@@ -19,6 +20,7 @@ export default function Home() {
         <Feed />
 
         {/* Widgets */}
+        <Widgets/>
 
         {/* Modal */}
       </main>
